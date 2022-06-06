71f312288c3570ce46e5b3ad565361b430fe53e4
Don <tuyishime.rwa@gmail.com>
2022-06-06 18:46:03 -0400

    restaurant route logic fix

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -2,7 +2,6 @@ const db = require("../db");
 const express = require("express");
 const router = express.Router();
 
-
 // Get all restaurants
 router.get("/", async (req, res) => {
   const dbResults = await db.query("SELECT * FROM restaurants;");
@@ -17,10 +16,9 @@ router.get("/", async (req, res) => {
 
 // Get a restaurant
 router.get("/:id", async (req, res) => {
-  const dbResults = await db.query(
-    "SELECT * FROM restaurants WHERE id = $1;",
-    [req.params.id]
-  );
+  const dbResults = await db.query("SELECT * FROM restaurants WHERE id = $1;", [
+    req.params.id,
+  ]);
 
   if (dbResults.rowCount !== 0) {
     res.json({
@@ -40,7 +38,7 @@ router.post("/", async (req, res) => {
   const dbResults = await db.query(
     `INSERT INTO restaurants (name, location, price_range)
       VALUES ($1, $2, $3) RETURNING *;`,
-      [req.query.name, req.query.location, req.query.price_range]
+    [req.body.name, req.body.location, req.body.price_range]
   );
 
   res.status(201).json({
@@ -57,7 +55,7 @@ router.put("/:id", async (req, res) => {
     `UPDATE restaurants
       SET name = $1, location = $2, price_range = $3
       WHERE id = $4 RETURNING *;`,
-      [req.query.name, req.query.location, req.query.price_range, req.params.id]
+    [req.body.name, req.body.location, req.body.price_range, req.params.id]
   );
 
   res.json({
@@ -73,11 +71,9 @@ router.delete("/:id", async (req, res) => {
   await db.query(
     `DELETE FROM restaurants
       WHERE id = $1;`,
-      [req.params.id]
+    [req.params.id]
   );
   res.status(204).send();
 });
-
-
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
-const restaurantsRouter = require('./routes/restaurants');
-
+const restaurantsRouter = require("./routes/restaurants");
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
+app.use(cors());
 app.use(express.json());
-app.use('/api/v1/restaurants', restaurantsRouter);
+app.use("/api/v1/restaurants", restaurantsRouter);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
