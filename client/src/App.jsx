f5dc7a9c83137523a7caf899e32dba2681bec27e
Don <tuyishime.rwa@gmail.com>
2022-06-06 18:47:28 -0400

    components + context + api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RestaurantDetails } from "./routes/RestaurantDetails";
 import { UpdateRestaurant } from "./routes/UpdateRestaurant";
-import { Home } from "./routes/Home";
+import Home from "./routes/Home";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route
-            exact
-            path="/restaurants/:id/update"
-            element={<UpdateRestaurant />}
-          />
-          <Route
-            exact
-            path="/restaurants/:id"
-            element={<RestaurantDetails />}
-          />
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              element={<UpdateRestaurant />}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              element={<RestaurantDetails />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 };
 
---
+++ client/src/components/AddRestaurant.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useContext } from "react";
+import RestaurantFinder from "../api/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+
+const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await RestaurantFinder.post("/", {
+        name,
+        location,
+        price_range: priceRange,
+      });
+      addRestaurants(response.data.data.restaurants[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="mb-4">
+      <form action="" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col">
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="name"
+            />
+          </div>
+          <div className="col">
+            <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="location"
+            />
+          </div>
+          <div className="col">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select form-control"
+            >
+              <option disabled>Price Range</option>
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+              <option value="5">$$$$$</option>
+            </select>
+          </div>
+          <button type="submit" className="col btn btn-primary">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddRestaurant;
